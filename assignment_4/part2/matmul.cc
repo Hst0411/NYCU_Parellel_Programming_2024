64c4ae43ea455b52f319bced31c56f44e96d46d0
Andy <73873452+Hst0411@users.noreply.github.com>
2024-11-15 18:31:58 +0800

    Update matmul.cc

--- assignment_4/part2/matmul.cc
+++ assignment_4/part2/matmul.cc
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <iostream>
 
+# include <stdio.h>
+# include <stdlib.h>
+
 #define MASTER  0
 #define FROM_MASTER 1 /* setting a message type */
 #define FROM_WORKER 2 /* setting a message type */
@@ -18,18 +21,23 @@ void construct_matrices(std::ifstream &in, int *n_ptr, int *m_ptr, int *l_ptr, i
         int n = *n_ptr;
         int m = *m_ptr;
         int l = *l_ptr;
+        printf("n: %d\n", n);
+        printf("m: %d\n", m);
+        printf("l: %d\n", l);
 
         *a_mat_ptr = (int *)malloc(n * m * sizeof(int));
         *b_mat_ptr = (int *)malloc(m * l * sizeof(int));
 
-        for(int i = 0; i < n*m; i++){
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
             // read ptr instead of address
-            in >> *a_mat_ptr[i];
+                in >> *a_mat_ptr[i * m + j];
+            }
         }
 
         for (int i = 0; i < m; i++){
             for (int j = 0; j < l; j++){
-                in >> *b_mat_ptr[j * m + i];
+                in >> *b_mat_ptr[i * l + j];
             }
         }
         /* test input
@@ -75,51 +83,58 @@ void matrix_multiply(const int n, const int m, const int l, const int *a_mat, co
 
         /* Send matrix data to the worker tasks */
         for(dest = 1; dest <= numworkers; dest++){
+            MPI_Send(&n, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
+            MPI_Send(&m, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
+            MPI_Send(&l, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
             rows = (dest <= extra) ? (averow + 1) : averow;
             MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
             MPI_Send(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            printf("master send info to rank %d\n", dest);
+            //printf("master send info to rank %d\n", dest);
             MPI_Send(&a_mat[offset*m], rows*m, MPI_INT, dest, mtype, MPI_COMM_WORLD);
             MPI_Send(&b_mat[0], m*l, MPI_INT, dest, mtype, MPI_COMM_WORLD);
             offset += rows;
         }
 
         /* Receive results from worker tasks */
-        int *c = (int *)malloc(n * l * sizeof(int));
+        int *c;
+        c = (int *)malloc(n * l * sizeof(int));
         mtype = FROM_WORKER;
         for (int i = 1; i <= numworkers; i++){
             source = i;
             MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
             MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
             MPI_Recv(&c[offset * l], rows*l, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &status);
-            printf("master receive from %d\n", source);
+            //printf("master receive from %d\n", source);
         }
         free(c);
     }
 
     /**************************** worker task ************************************/
     if(world_rank > MASTER){
-        int *a = (int *)malloc(n * m * sizeof(int));
-        int *b = (int *)malloc(m * l * sizeof(int));
-        int *c = (int *)malloc(n * l * sizeof(int));
+        int *a;
+        int *b;
+        int *c;
+        a = (int *)malloc(n * m * sizeof(int));
+        b = (int *)malloc(m * l * sizeof(int));
+        c = (int *)malloc(n * l * sizeof(int));
         mtype = FROM_MASTER;
         MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
         MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        printf("rank %d receive from master\n", world_rank);
+        //printf("rank %d receive from master\n", world_rank);
         MPI_Recv(&a[0], rows*m, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
         MPI_Recv(&b[0], m*l, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        printf("\n");
-        for (int i = 0; i < rows * m; i++) printf("a[%d]: %d\n", i, a[i]);
-        for (int i = 0; i < l * m; i++) printf("b[%d]: %d\n", i, b[i]);
-        printf("\n");
+        //printf("\n");
+        //for (int i = 0; i < rows * m; i++) printf("a[%d]: %d\n", i, a[i]);
+        //for (int i = 0; i < l * m; i++) printf("b[%d]: %d\n", i, b[i]);
+        //printf("\n");
 
         for (int k = 0; k < l; k++){
             for (int i = 0; i < rows; i++){
-                c[i * l + k] = 0.0;
+                c[i * l + k] = 0;
                 for (int j = 0; j < m; j++){
                     c[i * l + k] = c[i * l + k] + a_mat[i * m + j] * b_mat[j * l + k];
-                    printf("a[%d][%d] = %d\n", i, j, a[i*m + j]);
-		            printf("b[%d][%d] = %d\n", j, k, b[j*l + k]);
+                    //printf("a[%d][%d] = %d\n", i, j, a[i*m + j]);
+		            //printf("b[%d][%d] = %d\n", j, k, b[j*l + k]);
 
                 }
             }
@@ -129,7 +144,7 @@ void matrix_multiply(const int n, const int m, const int l, const int *a_mat, co
         MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
         MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
         MPI_Send(&c[0], rows*l, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-        printf("rank %d send result\n", world_rank);
+        //printf("rank %d send result\n", world_rank);
         free(a);
         free(b);
         free(c);
@@ -139,6 +154,11 @@ void matrix_multiply(const int n, const int m, const int l, const int *a_mat, co
 
 
 void destruct_matrices(int *a_mat, int *b_mat){
-    free(a_mat);
-    free(b_mat);
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == MASTER){
+        free(a_mat);
+        free(b_mat);
+    }
 }
