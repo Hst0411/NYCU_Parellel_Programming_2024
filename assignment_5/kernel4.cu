607e95933c52335199f95cd7f5acf23b4d608f35
Andy <73873452+Hst0411@users.noreply.github.com>
2024-11-27 23:14:33 +0800

    Update kernel4.cu

--- assignment_5/kernel4.cu
+++ assignment_5/kernel4.cu
@@ -1,21 +1,27 @@
+#pragma optimize(3, "Ofast", "inline")
 #include <cuda.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-__global__ void mandelKernel(int* DeviceMemory, int resX, float stepX, float stepY, float lowerX, float lowerY, int maxIterations) {
+template <int maxIterations>
+
+__global__ void mandelKernel(int* DeviceMemory, int resX, int resY, float lowerX, float lowerY, float dx, float dy){
     // To avoid error caused by the floating number, use the following pseudo code
     //
     // float x = lowerX + thisX * stepX;
     // float y = lowerY + thisY * stepY;
-    int thisX = blockIdx.x * blockDim.x + threadIdx.x;
-    int thisY = blockIdx.y * blockDim.y + threadIdx.y;
+	int thisX = blockIdx.x * blockDim.x + threadIdx.x;
+	int thisY = blockIdx.y * blockDim.y + threadIdx.y;
 
-    float c_re = lowerX + thisX * stepX;
-    float c_im = lowerY + thisY * stepY;
-    float z_re = c_re, z_im = c_im;
+    if (thisX >= resX || thisY >= resY) return;
+
+    float c_re = lowerX + thisX * dx;
+	float c_im = lowerY + thisY * dy;
+	float z_re = c_re, z_im = c_im;
 
     int i;
-    for (i = 0; i < maxIterations; ++i){
+    #pragma unroll
+    for (i = 0; i < maxIterations; ++i) {
 
         if (z_re * z_re + z_im * z_im > 4.f) break;
 
@@ -24,13 +30,40 @@ __global__ void mandelKernel(int* DeviceMemory, int resX, float stepX, float ste
         z_re = c_re + new_re;
         z_im = c_im + new_im;
     }
-    DeviceMemory[thisY * resX + thisX] = i;
+    DeviceMemory[thisX + thisY * resX] = i;
 }
 
-// Host front-end function that allocates the memory and launches the GPU kernel
+__global__ void mandelKernelDefault(int* DeviceMemory, int resX, int resY, float lowerX, float lowerY, float dx, float dy, int maxIterations){
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+    int thisX = blockIdx.x * blockDim.x + threadIdx.x;
+	int thisY = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (thisX >= resX || thisY >= resY) return;
+
+    float c_re = lowerX + thisX * dx;
+	float c_im = lowerY + thisY * dy;
+	float z_re = c_re, z_im = c_im;
+
+    int i;
+    #pragma unroll
+    for (i = 0; i < maxIterations; ++i) {
+        
+        if (z_re * z_re + z_im * z_im > 4.f) break;
+
+        float new_re = z_re * z_re - z_im * z_im;
+        float new_im = 2.f * z_re * z_im;
+        z_re = c_re + new_re;
+        z_im = c_im + new_im;
+    }
+    DeviceMemory[thisX + thisY * resX] = i;
+}
+
 void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
 {
-    float stepX = (upperX - lowerX) / resX;
+	float stepX = (upperX - lowerX) / resX;
     float stepY = (upperY - lowerY) / resY;
     int size = resX * resY * sizeof(int);
 
@@ -38,10 +71,26 @@ void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, i
     int *DeviceMemory;
     cudaMalloc(&DeviceMemory, size);
 
-    dim3 threadsPerBlock(16, 16);
-    dim3 numBlocks(resX / threadsPerBlock.x, resY / threadsPerBlock.y);
-    mandelKernel<<<numBlocks, threadsPerBlock>>>(DeviceMemory, resX, stepX, stepY, lowerX, lowerY, maxIterations);
+    dim3 blockSize(16, 16);
+    dim3 numBlocks(resX / blockSize.x, resY / blockSize.y);
 
+    switch (maxIterations) {
+        case 256:
+            mandelKernel<256><<<numBlocks, blockSize>>>(DeviceMemory, resX, resY, lowerX, lowerY, stepX, stepY);
+            break;
+        case 1000:
+            mandelKernel<1000><<<numBlocks, blockSize>>>(DeviceMemory, resX, resY, lowerX, lowerY, stepX, stepY);
+            break;
+        case 10000:
+            mandelKernel<10000><<<numBlocks, blockSize>>>(DeviceMemory, resX, resY, lowerX, lowerY, stepX, stepY);
+            break;
+        case 100000:
+            mandelKernel<100000><<<numBlocks, blockSize>>>(DeviceMemory, resX, resY, lowerX, lowerY, stepX, stepY);
+            break;
+        default:
+            mandelKernelDefault<<<numBlocks, blockSize>>>(DeviceMemory, resX, resY, lowerX, lowerY, stepX, stepY, maxIterations);
+            break;
+    }
     cudaMemcpy(img, DeviceMemory, size, cudaMemcpyDeviceToHost);
     //memcpy(img, HostMemory, size);
     
