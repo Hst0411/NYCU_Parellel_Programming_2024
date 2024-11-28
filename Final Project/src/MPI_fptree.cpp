71119610048b12119a02aa5bc7d29d9503819620
Andy <73873452+Hst0411@users.noreply.github.com>
2024-11-28 23:55:52 +0800

    Update MPI_fptree.cpp

--- Final Project/src/MPI_fptree.cpp
+++ Final Project/src/MPI_fptree.cpp
@@ -6,9 +6,27 @@
 #include <sstream>
 #include <mpi.h>
 #include <time.h>
+#include <vector>
+#include <cstring>
+using namespace std;
 
 #include "../include/fptree.hpp"
 
+void printFPTree(const std::shared_ptr<FPNode>& node, const std::string& indent = "") {
+    if (!node) return;
+
+    // print current node
+    if (node->item.empty()) {
+        std::cout << indent << "(Root)" << "\n";
+    } else {
+        std::cout << indent << "Item: " << node->item << ", Frequency: " << node->frequency << "\n";
+    }
+
+    // print child node recursively
+    for (const auto& child : node->children) {
+        printFPTree(child, indent + "  "); // 每層增加空白
+    }
+}
 
 FPNode::FPNode(const Item& item, const std::shared_ptr<FPNode>& parent) :
     item( item ), frequency( 1 ), node_link( nullptr ), parent( parent ), children()
@@ -38,9 +56,10 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
     int end_idx = start_idx + local_transaction_count + (world_rank < extra_transactions);
     //std::cout << "rank " << world_rank << ", end_idx: " << end_idx << '\n';
 
+    //double item_frequency_start_time = MPI_Wtime();
     // local data
     std::vector<Transaction> local_transactions(transactions.begin() + start_idx, transactions.begin() + end_idx);
-
+    /**/
     // Calculate local frequency
     std::map<Item, uint64_t> local_frequency_by_item;
     for (const Transaction& transaction : local_transactions) {
@@ -48,22 +67,23 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
             ++local_frequency_by_item[item];
         }
     }
+    
     /**/
     // Send local frequency
     std::map<Item, uint64_t> global_frequency_by_item;
     if (world_rank > 0){
-        /*
-        std::cout << "------------------------------------------------------" << std::endl;
-        std::cout << "Here is rank: " << world_rank << std::endl;
-        std::cout << "Counting the frequency of each item:" << std::endl;
-        for ( const Transaction& transaction : local_transactions ) {
-            for ( const Item& item : transaction ) {
+        
+        //std::cout << "------------------------------------------------------" << std::endl;
+        //std::cout << "Here is rank: " << world_rank << std::endl;
+        ///std::cout << "Counting the frequency of each item:" << std::endl;
+        //for ( const Transaction& transaction : local_transactions ) {
+            //for ( const Item& item : transaction ) {
                 // 計數每個 item 在 transaction 中的出現次數
-                std::cout << "item: " << item << " frequency: "  << local_frequency_by_item[item] << ", ";
-            }
-            std::cout << std::endl;
-        }
-        */
+                //std::cout << "item: " << item << " frequency: "  << local_frequency_by_item[item] << ", ";
+            //}
+            //std::cout << std::endl;
+        //}
+        
         for (const auto& pair : local_frequency_by_item) {
             const std::string& item = pair.first;
             uint64_t frequency = pair.second;
@@ -100,13 +120,19 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
                 global_frequency_by_item[item] += frequency;
             }
         }
-        /*
-        for (const auto& pair : global_frequency_by_item) {
-            std::cout << "item: " << pair.first << ", frequency: " << pair.second << '\n';
-        }
-        */
+        //for (const auto& pair : global_frequency_by_item) {
+            //std::cout << "item: " << pair.first << ", frequency: " << pair.second << '\n';
+        //}
     }
     MPI_Barrier(MPI_COMM_WORLD);
+    /*
+    if (world_rank == 0){
+        double item_frequency_end_time = MPI_Wtime();
+        std::cout << "Build frequency item set costs " << (item_frequency_end_time - item_frequency_start_time)*1000 << " ms\n";
+        std::cout << "Build frequency item set passed!" << std::endl;
+        std::cout << "------------------------------------------------------" << std::endl;
+    }
+    */
     /*
     // scan the transactions counting the frequency of each item
     // 計算每個 item 的 frequency
@@ -134,7 +160,8 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
         std::cout << "------------------------------------------------------" << std::endl;
     }
     */
-    /*
+
+    /**/
     // keep only items which have a frequency greater or equal than the minimum support threshold
     // Delete freq. < threshold 的 item
     for ( auto it = global_frequency_by_item.cbegin(); it != global_frequency_by_item.cend(); ) {
@@ -147,7 +174,93 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
             ++it;
         }
     }
+    
+    // Broadcast global frequency to all processors
+    if (world_rank == 0) {
+        // Serialize global_frequency_by_item into two arrays
+        std::vector<std::string> items;
+        std::vector<uint64_t> frequencies;
+        for (const auto& pair : global_frequency_by_item) {
+            items.push_back(pair.first);
+            frequencies.push_back(pair.second);
+        }
+
+        // Broadcast size
+        int size = items.size();
+        MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // Broadcast each item size and data
+        for (int i = 0; i < size; ++i) {
+            int item_length = items[i].size();
+            MPI_Bcast(&item_length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Bcast(const_cast<char*>(items[i].c_str()), item_length + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+        }
+
+        // Broadcast frequency
+        MPI_Bcast(frequencies.data(), size, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+    } else {
+        // Receive Broadcast data
+        int size;
+        MPI_Bcast(&size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        std::vector<std::string> items(size);
+        std::vector<uint64_t> frequencies(size);
+
+        for (int i = 0; i < size; ++i) {
+            int item_length;
+            MPI_Bcast(&item_length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+            char* buffer = new char[item_length + 1];
+            MPI_Bcast(buffer, item_length + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+            items[i] = buffer;
+            delete[] buffer;
+        }
+
+        MPI_Bcast(frequencies.data(), size, MPI_UINT64_T, 0, MPI_COMM_WORLD);
+
+        // Deserialize the received broadcast data back into a map
+        global_frequency_by_item.clear();
+        for (int i = 0; i < size; ++i) {
+            global_frequency_by_item[items[i]] = frequencies[i];
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    
+    /*
+    // verify that the broadcast is successful
+    if (world_rank == 0) {
+        std::cout << "Broadcasted global_frequency_by_item successfully!" << std::endl;
+    }
     */
+
+    /*
+    // transaction deleted low frequency
+    std::cout << "Rank " << world_rank << std::endl;
+    std::vector<Transaction> filtered_transactions;
+    for (const auto& transaction : transactions) {
+        Transaction filtered_transaction;
+        for (const auto& item : transaction) {
+            if (global_frequency_by_item.count(item) > 0) {
+                filtered_transaction.push_back(item);
+            }
+        }
+        if (!filtered_transaction.empty()) {
+            filtered_transactions.push_back(filtered_transaction);
+        }
+    }
+
+    // print transactions deleted low frequency
+    std::cout << "Transactions removed due to low frequency:\n";
+    for (size_t i = 0; i < filtered_transactions.size(); ++i) {
+        std::cout << "Transaction " << i + 1 << ": ";
+        for (const auto& item : filtered_transactions[i]) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "------------------------------------------------------" << std::endl;
+    */
+
     /*
     // test output
     std::cout << "Keep only items which have a frequency greater or equal than the minimum support threshold:" << std::endl;
@@ -159,7 +272,7 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
 
     // order items by decreasing frequency
     // 根據 item freq. 作 decreasing
-    /*
+    /**/
     struct frequency_comparator
     {
         bool operator()(const std::pair<Item, uint64_t> &lhs, const std::pair<Item, uint64_t> &rhs) const
@@ -168,22 +281,202 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
         }
     };
     std::set<std::pair<Item, uint64_t>, frequency_comparator> items_ordered_by_frequency(global_frequency_by_item.cbegin(), global_frequency_by_item.cend());
+    
+    /*
+    // print transactions sorted from each processors
+    std::cout << "Rank " << world_rank << " " << std::endl;
+    for (size_t i = 0; i < local_transactions.size(); ++i) {
+        const Transaction& transaction = local_transactions[i];
+        Transaction sorted_transaction;
+
+        // Filter and add items based on the order of items sorted by frequency
+        for (const auto& pair : items_ordered_by_frequency) {
+            const Item& item = pair.first;
+            if (std::find(transaction.cbegin(), transaction.cend(), item) != transaction.cend()) {
+                sorted_transaction.push_back(item);
+            }
+        }
+        // print transactions
+        std::cout << "Transaction " << i + 1 << ": ";
+        for (const auto& item : sorted_transaction) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "------------------------------------------------------" << std::endl;
     */
+
     // test output
     /*
     std::cout << "Order items by decreasing frequency:" << std::endl;
     for ( const auto& pair : items_ordered_by_frequency) {
         const uint64_t item_frequency = pair.second;
-        std::cout << "First: " << pair.first << " , item_frequency: " << item_frequency << std::endl;
+        std::cout << "Item: " << pair.first << ", item_frequency: " << item_frequency << std::endl;
     }
     std::cout << "------------------------------------------------------" << std::endl;
     */
-    
-    // start tree construction
 
+    double tree_start_time = MPI_Wtime();
+
+    // declare local root and local header table for each threads
+    vector<shared_ptr<FPNode>> local_roots(world_size, nullptr);
+    vector<map<Item, shared_ptr<FPNode>>> local_header_tables(world_size);
+
+
+    // initialize local root for each threads
+    for (int idx = 0; idx < world_size; idx++) {
+        local_roots[idx] = make_shared<FPNode>(Item{}, nullptr);
+    }
+
+    // get local root
+    auto& local_root = local_roots[world_rank];
+
+    // get local header table 
+    auto& local_header_table= local_header_tables[world_rank];
+
+    // construct a local FP-tree for each processors
+    for ( size_t i = 0; i < transactions.size(); i++ ) {
+
+        const Transaction& transaction = transactions[i];
+        // 從 root start
+        auto curr_fpnode = local_root;
+
+        // select and sort the frequent items in transaction according to the order of items_ordered_by_frequency
+        for ( const auto& pair : items_ordered_by_frequency ) {
+            const Item& item = pair.first;
+
+            // check if item is contained in the current transaction
+            // 檢查目前交易中是否包含此 item
+            if ( std::find( transaction.cbegin(), transaction.cend(), item ) != transaction.cend() ) {
+                // insert item in the tree
+                // 如果包含該 item，insert 到 FP-tree
+
+                // check if curr_fpnode has a child curr_fpnode_child such that curr_fpnode_child.item = item
+                // 檢查目前節點是否已經有該 item 的子節點
+                const auto it = std::find_if(
+                    curr_fpnode->children.cbegin(), curr_fpnode->children.cend(),  [item](const std::shared_ptr<FPNode>& fpnode) {
+                        return fpnode->item == item;
+                } );
+                if ( it == curr_fpnode->children.cend() ) {
+                    // the child doesn't exist, create a new node
+                    // 子節點不存在，create 新節點
+                    const auto curr_fpnode_new_child = std::make_shared<FPNode>( item, curr_fpnode );
+
+                    // add the new node to the tree
+                    // 將 new 節點添加到當前節點的子節點列表中
+                    curr_fpnode->children.push_back( curr_fpnode_new_child );
+
+                    // update the node-link structure
+                    // 更新 Node-Link 結構
+                    if ( local_header_table.count( curr_fpnode_new_child->item ) ) {
+                        auto prev_fpnode = local_header_table[curr_fpnode_new_child->item];
+                        while ( prev_fpnode->node_link ) { 
+                            // 遍歷到該項目最後一個節點
+                            prev_fpnode = prev_fpnode->node_link;
+                        }
+                        // 建立 link
+                        prev_fpnode->node_link = curr_fpnode_new_child;
+                    }
+                    else {
+                        // 添加 header link
+                        local_header_table[curr_fpnode_new_child->item] = curr_fpnode_new_child;
+                    }
+
+                    // advance to the next node of the current transaction
+                    // 移動到新建的子節點
+                    curr_fpnode = curr_fpnode_new_child;
+                }
+                else {
+                    // the child exist, increment its frequency
+                    // 子節點已存在，增加 frequency
+                    auto curr_fpnode_child = *it;
+                    ++curr_fpnode_child->frequency;
+
+                    // advance to the next node of the current transaction
+                    // 移動到已存在的子節點
+                    curr_fpnode = curr_fpnode_child;
+                }
+            }
+        }
+    }
+    /**/
+    // merge header_table
+    if (world_rank == 0) {
+        // Master processor receives the header_table of all processors
+        for (int source = 1; source < world_size; ++source) {
+            uint64_t num_items;
+            MPI_Recv(&num_items, 1, MPI_UINT64_T, source, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            for (uint64_t i = 0; i < num_items; ++i) {
+                uint64_t item_length;
+                MPI_Recv(&item_length, 1, MPI_UINT64_T, source, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                std::vector<char> buffer(item_length);
+                MPI_Recv(buffer.data(), item_length, MPI_CHAR, source, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                uint64_t frequency;
+                MPI_Recv(&frequency, 1, MPI_UINT64_T, source, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+                std::string item(buffer.data());
+                if (header_table.count(item)) {
+                    auto node = header_table[item];
+                    while (node->node_link) node = node->node_link;
+                    node->node_link = std::make_shared<FPNode>(item, nullptr);
+                    node->node_link->frequency = frequency;
+                } else {
+                    auto new_node = std::make_shared<FPNode>(item, nullptr);
+                    new_node->frequency = frequency;
+                    header_table[item] = new_node;
+                }
+            }
+        }
+    } else {
+        // Each processor sends header_table
+        uint64_t num_items = header_table.size();
+        MPI_Send(&num_items, 1, MPI_UINT64_T, 0, 0, MPI_COMM_WORLD);
+
+        for (const auto& pair : header_table) {
+            const std::string& item = pair.first;
+            uint64_t frequency = pair.second->frequency;
+
+            uint64_t item_length = item.size() + 1;  // 包含結尾 '\0'
+            MPI_Send(&item_length, 1, MPI_UINT64_T, 0, 1, MPI_COMM_WORLD);
+            MPI_Send(item.c_str(), item_length, MPI_CHAR, 0, 2, MPI_COMM_WORLD);
+            MPI_Send(&frequency, 1, MPI_UINT64_T, 0, 3, MPI_COMM_WORLD);
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    /**/
+    if (world_rank == 0){
+        double tree_end_time = MPI_Wtime();
+        std::cout << "Build FP-tree costs " << (tree_end_time - tree_start_time)*1000 << " ms\n";
+        std::cout << "Build FP-tree passed!" << std::endl;
+        //std::cout << "------------------------------------------------------" << std::endl;
+        //std::cout << "Final FP-tree structure:\n";
+        //printFPTree(local_root);
+    }
+    
+    // select and sort the frequent items in transaction according to the order of items_ordered_by_frequency
+    /*
+    std::cout << "Item Frequencies in the FP-tree:\n";
+    for (const auto& pair : header_table) {
+        const Item& item = pair.first;
+        auto node = pair.second;
+        // std::cout << node->item << ": " << "\n";
+        // 累積該 item 的頻率
+        uint64_t total_frequency = 0;
+        while (node != nullptr) {
+            total_frequency += node->frequency;
+            node = node->node_link;
+        }
+        std::cout << "Item: " << item << ", Frequency: " << total_frequency << "\n";
+    }
+    */
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // start tree construction
     // scan the transactions again
     // 構建 FP-tree
-    /**/
+    /*
     for ( const Transaction& transaction : transactions ) {
         // 從 root start
         auto curr_fpnode = root;
@@ -245,22 +538,6 @@ FPTree::FPTree(const std::vector<Transaction>& transactions, uint64_t minimum_su
                 }
             }
         }
-    }
-    
-    // select and sort the frequent items in transaction according to the order of items_ordered_by_frequency
-    /*
-    std::cout << "Item Frequencies in the FP-tree:\n";
-    for (const auto& pair : header_table) {
-        const Item& item = pair.first;
-        auto node = pair.second;
-        std::cout << node->item << ": " << "\n";
-        // 累積該 item 的頻率
-        uint64_t total_frequency = 0;
-        while (node != nullptr) {
-            total_frequency += node->frequency;
-            node = node->node_link;
-        }
-        std::cout << "Item: " << item << ", Frequency: " << total_frequency << "\n";
     }
     */
 }
